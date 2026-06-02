f3866af27e3a7aa1f0247a5f162918fba5767228
santofemiano <santofemiano7@gmail.com>
2026-06-02 03:20:04 +0200

    initialize project structure with Maven, add application properties, and create main application class

---
+++ src/main/java/com/santofem/redditoauto/RedditoAutoApplication.java
@@ -0,0 +1,13 @@
+package com.santofem.redditoauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditoAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditoAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/santofem/redditoauto/RedditoAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santofem.redditoauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditoAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
